edc7c0e6e03ba5c84a2d839e61d941df8e54cbe8
Shahd Abass <shahdabass94@gmail.com>
2024-04-19 11:45:12 -0700

    push5

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,46 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require('../vendor/smtp/PHPMailer.php');
+require('../vendor/smtp/SMTP.php');
+require('../vendor/smtp/Exception.php');
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+require('vendor/smtp/PHPMailer.php');
+require('vendor/smtp/SMTP.php');
+require('vendor/smtp/Exception.php');
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\SMTP;
+ use PHPMailer\PHPMailer\Exception;
+ $mail = new PHPMailer(true);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+ try {
+   //Server settings
+   $mail->SMTPDebug = SMTP::DEBUG_SERVER;          
+   $mail->isSMTP();     
+   $mail->SMTPAuth   = true;                       
+   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+   $mail->Port       = 587;        
+   $mail->SMTPSecure = "tls";                  
 
-  echo $contact->send();
+   $mail->Host       = 'smtp.gmail.com';
+   $mail->Username   = 'info@trkez.com';
+   $mail->Password   = 'egrbtxdkihq';
+   
+   $mail->setFrom('info@trkez.com', 'Trkez');
+   $mail->addAddress('info@trkez.com');
+
+   $mail->isHTML(true);
+   $mail->Subject = 'Notification: Received New Inquiry!';
+   $mail->Body    = 'Name: ' . $_POST['name'] . '<br>' . 
+                    'Email: ' . $_POST['email'] . '<br>'. 
+                    'Subject: ' . $_POST['subject'] . '<br>' .
+                    'Message: '. $_POST['message'];
+
+   $mail->send();
+   echo 'Message has been sent';
+} catch (Exception $e) {
+   echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+ $retval = mail($to,$subject,$message,$header);
 ?>
